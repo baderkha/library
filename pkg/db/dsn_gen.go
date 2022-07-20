40397f80636e12eddb71ce5d8eddeee18469dc99
ahmad <ahmad_baderkhan@pop-os.localdomain>
2022-07-20 17:58:22 -0400

    done

---
+++ pkg/db/dsn_gen.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	DialectMYSQL = "MYSQL"
+)
+
+// GetDSN : generate dsn string for the correct sql dialect
+func GetDSN(dialect string, host string, username string, password string, port string, database string, queryParam ...string) string {
+	switch dialect {
+	case DialectMYSQL:
+		return fmt.Sprintf(`%s:%s@%s:%s/%s?%s`, username, password, host, port, database, strings.Join(queryParam, "&"))
+	default:
+		return ""
+	}
+}
